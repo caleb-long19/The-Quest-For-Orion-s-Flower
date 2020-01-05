54dc38ee03b7640857ca7051a10407df1a656dca
caleb-long19 <caleblong199@gmail.com>
2020-01-05 05:27:08 +0000

    The Quest For Orion’s Flower - Feedback Improvement Update (1)
    First update to the 2D Unity Project!
    
    List of Changes/Additions:
    Zombie Speed changed from 2 to 8.
    Camera has been zoomed out.
    
    Bug Fixes
    Player Health works as intended. When the player collides with a zombie, the health bar now decreases.

---
+++ The Quest For Orion's Flower/Assets/Scripts/DestroyOnDie.cs
@@ -0,0 +1,9 @@
+﻿using UnityEngine;
+
+public class DestroyOnDie : MonoBehaviour
+{
+    public void Die()
+    {
+        Destroy(gameObject);
+    }
+}
---
+++ The Quest For Orion's Flower/Assets/Scripts/HurtTrigger.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class HurtTrigger : MonoBehaviour
+{
+    public int damage;
+    public float resetTime = 0.25f;
+
+    private void OnTriggerEnter2D(Collider2D collision)
+    {
+        collision.transform.SendMessage("TakeDamage", damage, SendMessageOptions.DontRequireReceiver);
+        GetComponent<Collider2D>().enabled = false;
+        Invoke("ResetTrigger", resetTime);
+    }
+
+    private void ResetTrigger()
+    {
+        GetComponent<Collider2D>().enabled = true;
+    }
+}
