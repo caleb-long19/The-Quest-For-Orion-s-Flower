caec77458ffc3b28d72146e4c498e203b2c8b557
caleb-long19 <caleblong199@gmail.com>
2020-01-05 23:48:12 +0000

    The Quest For Orion’s Flower - Quality of Life 2.0 Update (15) - Reupload
    Reupload due to project issues involving merging two unity projects!
    
    List of Changes/Additions:
    Improved Combat - Player can now swing their Sword while moving!
    Updated Game Over UI!
    Increased the speed of Enemies!
    
    -------------------------------------------------
    
    Bug Fixes
    Players Health, Shield, Score, Ammo doesn’t reset when the Player restarts the game! - Fixed
    Enemies go through every object - Fixed
    
    -------------------------------------------------
    
    Code Improvements
    Added & Updated Code Comments
    
    -------------------------------------------------
    
    Audio
    Removed Some Sound Effects
    Replaced “Coin” SFX With my own
    Replaced “Key Pickup” SFX With my own
    Replaced “In Game” Music
    
    -------------------------------------------------
    
    Animations
    Added “Clark_Walking_Sword_Up” Animation
    Added “Clark_Walking_Sword_Down” Animation
    Added “Clark_Walking_Sword_Left” Animation
    Added “Clark_Walking_Sword_Right” Animation
    
    -------------------------------------------------
    
    Scripts
    Updated “Ammo” Script
    Updated “GameOver” Script
    Updated “Dialog” Script
    Updated “HealthSystem” Script
    Updated “Coin” Script
    Updated “KeyPickup” Script

---
+++ The Quest for Orion's Flower/Assets/Scripts/Scripts/Weapons/RangedAttack.cs
@@ -0,0 +1,36 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class RangedAttack : MonoBehaviour
+{
+    public GameObject Blood; // Blood particles for enemy
+    public int damage = 5;
+
+    public float arrowSpeed;
+    public Rigidbody2D myRigidbody2D;
+
+
+    public void Setup(Vector2 velocity, Vector3 direction)
+    {
+        myRigidbody2D.velocity = velocity.normalized * arrowSpeed;
+        transform.rotation = Quaternion.Euler(direction);
+    }
+
+    private void OnTriggerEnter2D(Collider2D other)
+    {
+        other.transform.SendMessage("TakeDamage", damage, SendMessageOptions.DontRequireReceiver);
+        Die();
+    }
+
+    private void OnBecameInvisible()
+    {
+        Die();
+    }
+
+    private void Die()
+    {
+        Destroy(gameObject);
+    }
+
+}
---
+++ The Quest for Orion's Flower/Assets/Scripts/Scripts/World/CrateButton.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CrateButton : MonoBehaviour
+{
+    public GameObject Barrier;
+    public AudioSource Press;
+
+    public Animator ButtonAnimationController;
+
+    void OnTriggerStay2D(Collider2D collision)
+    {
+        if (collision.gameObject.tag.Equals("Crate"))
+        {
+            Barrier.SetActive(false); // Barrier GameObject is set to Inactive
+            ButtonAnimationController.SetTrigger("isPressed"); // If the Crate collides with the button, switch button to isPressed in the Animator
+
+            Debug.Log("Crate Button has been Pressed");
+        }
+    }
+
+    private void OnTriggerExit2D(Collider2D collision)
+    {
+        Barrier.SetActive(true); // Barrier GameObject is set to active
+        ButtonAnimationController.SetTrigger("isNotPressed"); // If the Crate isn't colliding with the button, switch button to isNotPressed in the Animator
+    }
+}
