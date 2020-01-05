bee2f4d5f1f9ef4b0ecb16bf31db82013866d160
caleb-long19 <caleblong199@gmail.com>
2020-01-05 05:52:47 +0000

    The Quest For Orion’s Flower - Player Animations Update (8)
    List of Changes/Additions:
    Added Animations to Player Character (Move Up, Down , Left, Right, and Idle)
    W,A,S,D key presses now play the correct animations
    Improved the pivot positions on multiple assets to work with the new animations
    Improved the Desert Biome slightly
    
    -------------------------------------------------
    
    Sprites
    Added “ClarkFront” Sprite
    Added “ClarkBack” Sprite
    Added “ClarkLeftSide” Sprite
    Added “ClarkRightSide” Sprite
    Added “Clark_Walking_Up” Sprites
    Added “Clark_Walking_Down” Sprites
    Added “Clark_Walking_Left” Sprites
    Added “Clark_Walking_Right” Sprites
    
    -------------------------------------------------
    
    Animations
    Updated “Clark_Idle” Animation
    Added “Clark_Walking_Up” Animation
    Added “Clark_Walking_Down” Animation
    Added “Clark_Walking_Right” Animation
    Added “Clark_Walking_Leftt” Animation

--- The Quest For Orion's Flower/Assets/Scripts/Exercise Sheet Scripts/TopDownCharacterController2D.cs
+++ The Quest For Orion's Flower/Assets/Scripts/Exercise Sheet Scripts/TopDownCharacterController2D.cs
@@ -7,34 +7,29 @@ using System.Collections.Generic;
 public class TopDownCharacterController2D : MonoBehaviour
 {
     public float speed = 5.0f;
-    AnimatorController myAnim;
-    private Animator PlayerAnim;
     Rigidbody2D rigidBody2D;
+    public Animator PlayerMovement;
+
+    Vector2 Movement;
 
     // Start is called before the first frame update
     void Start()
     {
         rigidBody2D = GetComponent<Rigidbody2D>();
-        myAnim = AnimatorController.instance;
-        PlayerAnim = GetComponent<Animator>();
     }
 
     // Update is called once per frame
     void FixedUpdate()
     {
-        float x = Input.GetAxis("Horizontal");
-        float y = Input.GetAxis("Vertical");
+        Movement.x = Input.GetAxis("Horizontal");
+        Movement.y = Input.GetAxis("Vertical");
 
-        rigidBody2D.velocity = new Vector2(x, y) * speed;
+        PlayerMovement.SetFloat("Horizontal", Movement.x);
+        PlayerMovement.SetFloat("Vertical", Movement.y);
+        PlayerMovement.SetFloat("Speed", Movement.sqrMagnitude);
+
+        rigidBody2D.velocity = new Vector2(Movement.x, Movement.y) * speed;
         rigidBody2D.angularVelocity = 0.0f;
 
-        if (y == 0 && x == 0)
-        {
-            PlayerAnim.SetBool("isWalking", false);
-        }
-        else
-        {
-            PlayerAnim.SetBool("isWalking", true);
-        }
     }
 }
